2fb62c7aca1537a116d29f5f9cc19263c0bfcda9
Vinay <itsvinayadav@gmail.com>
2024-08-18 18:21:01 +0530

    typo correction

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { PORT } from './env.js';
-import middlewware from './middleware/middlewware.js';
+import middleware from './middleware/middlewware.js';
 import route from './setup/parentRoute.js';
 
 
 const app = express()
 
-middlewware(app)
+middleware(app)
 route(app)
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
